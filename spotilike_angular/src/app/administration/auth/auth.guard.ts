91ef8b65de0931e27f8646005aac676ae1875a37
Julien Lay <julien-lay@outlook.fr>
2024-04-17 14:53:49 +0200

    suppression des imports inutiles

--- spotilike_angular/src/app/administration/auth/auth.guard.ts
+++ spotilike_angular/src/app/administration/auth/auth.guard.ts
@@ -1,12 +1,5 @@
 
 import { CanActivateFn } from '@angular/router';
-import { inject } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  createUrlTreeFromSnapshot,
-} from '@angular/router';
-import { map } from 'rxjs';
-import { AuthService } from './auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   if (localStorage.getItem('token')) {
